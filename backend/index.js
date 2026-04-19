274bc8dc2e20345365f4e2f05611b13095fc106a
Pranav Bankar <pranavbankar124@gmail.com>
2026-04-19 13:16:29 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express"
+const app = express();
+const PORT =3000;
+app.use (express.json());
+
+
+app.get('/',(req,res) => {
+   console.log("got a request ");
+   
+    
+})
+
+app.listen(PORT,()=>{
+    console.log("SERVER ARE STARTED AT PORT NO :",PORT);
+})
