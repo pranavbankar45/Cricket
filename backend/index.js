b92d082425e643c8cd14be78b8d9ea12bb7ddc21
Mrunal-Malkar <mrunalpmalkar@gmail.com>
2026-05-02 17:19:41 +0530

    initial backend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,19 @@
 import express from "express"
+import cors from "cors"
+
 const app = express();
 const PORT =3000;
 app.use (express.json());
+app.use(cors());
 
 
 app.get('/',(req,res) => {
    console.log("got a request ");
-   
-    
+})
+
+app.get("/test",(req,res)=>{
+    console.log("got a request at test route");
+    res.json({message:"Hello from test route"});
 })
 
 app.listen(PORT,()=>{
--- frontend/@latest/src/App.tsx
+++ frontend/@latest/src/App.tsx
@@ -1,10 +1,25 @@
 
+import { useEffect } from 'react';
 import './App.css'
 // import Login from "./Pages/Login.tsx"
 // import Profile from "./Pages/Profile."
 import CreateProfile from "./Pages/CreateProfile.tsx"
 function App() {
 
+  async function test(){
+    const res=await fetch("http://localhost:3000/test",{
+      method:"GET",
+      headers:{
+        "Content-Type":"application/json",
+        body:JSON.stringify({data:"Hello world"})
+      }
+    })
+    console.log("res",res);
+  }
+
+  useEffect(()=>{
+    test();
+  },[])
 
   return (
     <>
